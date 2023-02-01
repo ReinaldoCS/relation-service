468d98bee6ce3a3e7706bfef94d0d7696f0a8fed
Reinaldo Santos <santosdoscorreiareinaldo@gmail.com>
2023-02-01 15:53:33 -0300

    Feature: criando roda de reset de senha

--- src/infra/http/controllers/users.controller.ts
+++ src/infra/http/controllers/users.controller.ts
@@ -4,15 +4,22 @@ import {
   Controller,
   HttpStatus,
   InternalServerErrorException,
+  Patch,
   Post,
+  Req,
 } from '@nestjs/common';
 import { RegisterUser } from '@application/use-cases/register-user';
 import { CreateUserBody } from '../dtos/create-user-body';
 import { AppException } from '@helpers/AppException';
+import { ResetPassword } from '@application/use-cases/reset-password';
+import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
-  constructor(private registerUser: RegisterUser) {}
+  constructor(
+    private registerUser: RegisterUser,
+    private resetPassword: ResetPassword,
+  ) {}
 
   @Post()
   async create(@Body() body: CreateUserBody) {
@@ -28,6 +35,33 @@ export class UsersController {
       return {
         user,
       };
+    } catch (error) {
+      if (error instanceof AppException) {
+        throw new BadRequestException({
+          status: HttpStatus.BAD_REQUEST,
+          error: error.message,
+        });
+      }
+
+      const errorMessage = (error as Error).message;
+      throw new InternalServerErrorException(errorMessage);
+    }
+  }
+
+  @Patch('reset')
+  async password(@Req() request: Request, @Body() body: { password: string }) {
+    try {
+      const { password } = body;
+      const email = request.headers['x-querystring'];
+
+      if (!email) {
+        throw new AppException('X-QueryString header with email not found.');
+      }
+
+      await this.resetPassword.execute({
+        email: email.toString(),
+        newPassword: password,
+      });
     } catch (error) {
       if (error instanceof AppException) {
         throw new BadRequestException({
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RegisterUser } from '@application/use-cases/register-user';
 import { DatabaseModule } from '../database/database.module';
 import { UsersController } from './controllers/users.controller';
+import { ResetPassword } from '@application/use-cases/reset-password';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [RegisterUser],
+  providers: [RegisterUser, ResetPassword],
 })
 export class HttpModule {}
